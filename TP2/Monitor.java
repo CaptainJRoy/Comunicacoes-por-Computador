1f193bb180ede1189c4433f2830c7cbfd9d2bcf2
core <core@XubunCORE.(none)>
2017-05-07 18:28:52 +0100

    Nova versão do TP2, com alterações no UDP e acrescentado TCP

---
+++ TP2/Monitor.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class Monitor
+{
+	public static void main(String args[]) throws Exception
+	{
+		DatagramSocket socket = new DatagramSocket(5555);
+		InetAddress ipAddress = InetAddress.getByName(args[0]); //ReverseProxy
+		Thread registo = new Thread(new Registo(ipAddress, socket));
+		Thread pooling = new Thread(new Pooling(ipAddress, socket));
+		registo.start();
+		pooling.start();
+		Thread tcp = new Thread(new MonitorTCP());
+		tcp.start();
+	}
+}
+
+class MonitorTCP implements Runnable{
+
+	public void run(){
+		try{
+			ServerSocket servidor = new ServerSocket(80);
+			while(true){
+				Socket cliente = servidor.accept();
+				Thread t = new Thread(new Cliente(cliente));
+				t.start();
+			}
+		}
+		catch(IOException e){} 
+
+	}
+}
+
+class Cliente implements Runnable{
+
+	Socket cliente;
+
+	Cliente(Socket cliente){
+		this.cliente = cliente;
+	}
+
+	public void run(){
+		try{
+			BufferedReader fromCliente = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+			PrintWriter toCliente = new PrintWriter(cliente.getOutputStream());
+			while(true){
+				String s = fromCliente.readLine();
+				if (s == null)
+					break;
+				
+				toCliente.println(s.toUpperCase());
+				toCliente.flush();
+			}
+			cliente.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
+
+class Registo implements Runnable{
+	DatagramPacket packet;
+	DatagramSocket socket;
+
+	Registo (InetAddress ipAddress, DatagramSocket socket) throws SocketException{
+		this.socket = socket;
+		byte[] sendData = new byte[1024];
+		String sentence = "registo";
+		sendData = sentence.getBytes();
+		packet = new DatagramPacket (sendData, sendData.length, ipAddress, 5555);
+	}
+
+	public void run(){
+		while(true){
+			try{
+				socket.send(packet);
+				Thread.sleep(5000);
+			}
+			catch (IOException | InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Pooling implements Runnable{
+
+	DatagramSocket socket;
+	InetAddress ipAddress;
+
+	Pooling (InetAddress ipAddress, DatagramSocket socket){
+		this.socket = socket;
+		this.ipAddress = ipAddress;
+	}
+
+	public void run(){
+		try{
+			byte[] receiveData = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			String sentence;
+			while(true){
+				socket.receive(receivePacket);
+				sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+				String[] pdu = sentence.split(" ");
+
+				if (pdu[0].equals("pedido")){
+					enviarInformacao(pdu[1], pdu[2]);
+				}
+
+				//System.out.println("RECEIVED: " + sentence);
+			}
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	void enviarInformacao(String id, String tempoE){
+		try{
+			long tempoInicial = (new Date()).getTime();
+			byte[] sendData = new byte[1024];
+
+			Process proc = Runtime.getRuntime().exec(new String[] {"bash", "-c", "netstat -an | grep tcp | wc -l"});
+			BufferedReader br = new BufferedReader (new InputStreamReader (proc.getInputStream()));
+
+			long tempoFinal = (new Date()).getTime();
+			long tempoEnvio = Long.parseLong(tempoE);
+			String sentence = "resposta " + id + " " + br.readLine() + " " + Long.toString(tempoFinal - tempoInicial + tempoEnvio);
+			sendData = sentence.getBytes();
+			DatagramPacket packet = new DatagramPacket(sendData, sendData.length, ipAddress, 5555);
+			socket.send(packet);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+}
--- TP2/ReverseProxy.java
+++ TP2/ReverseProxy.java
@@ -2,52 +2,144 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-class ReverseProxy
-{
-	public static void main(String args[]) throws Exception
-		{
-			Tabela informacoes = new Tabela();
-			Gestor gestor = new Gestor(informacoes);
-			gestor.start();
-			DatagramSocket socket = new DatagramSocket(5555);
-			byte[] receiveData = new byte[1024];
-				while(true)
-					{
-						DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-						socket.receive(receivePacket);
-						String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
-
-						String[] pdu = sentence.split(" ");
-
-						if (pdu[0].equals("registo")){
-							enviarPedido(socket, receivePacket, informacoes);
-						}
-						else if (pdu[0].equals("resposta")){
-							atualizarInformacao(receivePacket, informacoes, pdu[1]);
-						}
-						System.out.println("RECEBIDO: " + sentence);
-					}
-		}
-
-		static void enviarPedido(DatagramSocket socket, DatagramPacket receivePacket, Tabela informacoes){
-			InetAddress ipAddress;
-			try{
-				byte[] sendData = new byte[1024];
-				ipAddress = receivePacket.getAddress();
-				int port = receivePacket.getPort();
-				String sentence = "pedido " + informacoes.enviar(ipAddress);
-				sendData = sentence.getBytes();
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
-				socket.send(sendPacket);
-			}
-			catch (IOException e){}
-		}
-
-		static void atualizarInformacao(DatagramPacket receivePacket, Tabela informacoes, String id){
-			InetAddress ipAddress = receivePacket.getAddress();
-			informacoes.recebido(ipAddress, id);
-		}
+class ReverseProxy{
+	
+	public static void main(String args[]) throws Exception{
+		Tabela informacoes = new Tabela();
+		
+		Thread monitorizacao = new Thread(new Monitorizacao(informacoes));
+		monitorizacao.start();
+		
+		Thread proxyTCP = new Thread(new ProxyTCP(informacoes));
+		proxyTCP.start();
+	}
 }
+
+class Monitorizacao implements Runnable{
+	Tabela informacoes;
+
+	Monitorizacao(Tabela informacoes){
+		this.informacoes = informacoes;
+	}
+
+	public void run(){
+		try{
+			DatagramSocket socket = new DatagramSocket(5555);
+			while(true){
+				DatagramPacket receberPacote = receberPacote(socket);
+				String dados = new String(receberPacote.getData(), 0, receberPacote.getLength());
+				String[] pdu = dados.split(" ");
+				if(pdu[0].equals("resposta")){
+					atualizarInformacao(receberPacote, pdu);
+				}
+				else if(pdu[0].equals("registo")){
+					enviarPedido(socket, receberPacote);
+				}
+			}
+		}
+		catch(SocketException e){
+			e.printStackTrace();
+		}
+	}
+
+	DatagramPacket receberPacote(DatagramSocket socket){
+		try{
+			byte[] receberDados = new byte[1024];
+			DatagramPacket receberPacote = new DatagramPacket(receberDados, receberDados.length);
+			socket.receive(receberPacote);
+			return receberPacote;
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	void enviarPedido(DatagramSocket socket, DatagramPacket receberPacote){
+		InetAddress ipAddress;
+		try{
+			byte[] enviarDados = new byte[1024];
+			ipAddress = receberPacote.getAddress();
+			int port = receberPacote.getPort();
+			String dados = "pedido " + informacoes.enviar(ipAddress);
+			enviarDados = dados.getBytes();
+			DatagramPacket enviarPacote = new DatagramPacket(enviarDados, enviarDados.length, ipAddress, port);
+			socket.send(enviarPacote);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	void atualizarInformacao(DatagramPacket receberPacote, String[] pdu){
+		InetAddress ipAddress = receberPacote.getAddress();
+		informacoes.recebido(ipAddress, pdu[1], pdu[2], pdu[3]);
+	}
+}
+
+class ProxyTCP implements Runnable{
+
+	Tabela informacoes;
+
+	ProxyTCP(Tabela informacoes){
+		this.informacoes = informacoes;
+	}
+
+	public void run(){
+		try{
+			ServerSocket servidor = new ServerSocket(80);
+			while(true){
+				Socket cliente = servidor.accept();
+				InetAddress ipAddress = informacoes.getMonitor();
+				Thread c = new Thread(new Intermediacao(cliente, ipAddress));
+				c.start();
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
+
+class Intermediacao implements Runnable{
+
+	Socket cliente;
+	InetAddress ipAddress; // monitor
+
+	Intermediacao (Socket cliente, InetAddress ipAddress){
+		this.cliente = cliente;
+		this.ipAddress = ipAddress;
+	}
+
+	public void run(){
+		try{
+			BufferedReader fromCliente = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+			PrintWriter toCliente = new PrintWriter(cliente.getOutputStream());
+			Socket monitor = new Socket(ipAddress, 80);
+			BufferedReader fromMonitor = new BufferedReader(new InputStreamReader(monitor.getInputStream()));
+			PrintWriter toMonitor = new PrintWriter(monitor.getOutputStream());
+			while(true){
+				String s = fromCliente.readLine();
+				toMonitor.println(s);
+				toMonitor.flush();
+
+				if (s == null)
+					break;
+
+				s = fromMonitor.readLine();
+				toCliente.println(s);
+				toCliente.flush();
+
+			}
+			cliente.close();
+			monitor.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
+
 
 class Tabela{
 
@@ -66,13 +158,35 @@ class Tabela{
 		return s.enviar();
 	}
 
-	void recebido(InetAddress ipAddress, String id){
+	void recebido(InetAddress ipAddress, String id, String nTCP, String tempo){
 		Servidor s = servidores.get(ipAddress);
-		s.recebido(id);
+		s.recebido(id, nTCP, tempo);
+		imprimirTabela();
 	}
 
-	void remover(InetAddress ipAddress){
+	/*void remover(InetAddress ipAddress){
 		servidores.remove(ipAddress);
+	}*/
+
+	InetAddress getMonitor(){
+		InetAddress ipAddress = null;
+		float prioridade = Float.MAX_VALUE;
+		for (Servidor s : servidores.values()){
+			float p = s.getPrioridade();
+			if (p < prioridade){
+				prioridade = p;
+				ipAddress = s.getIpAddress();
+			}
+		}
+		imprimirTabela();
+		return ipAddress;
+	}
+
+	void imprimirTabela(){
+		System.out.println("IP - RTT - Taxa - Conexões");
+		for (Servidor s : servidores.values()){
+			s.imprimirServidor();
+		}
 	}
 }
 
@@ -80,56 +194,61 @@ class Servidor{
 
 	InetAddress ipAddress;
 	float rtt;
-	int totalEnviados; 
-	int totalRecebidos;
-	long ultimoEnviado;
+	int totalEnviados;
 	int conexoes;
 
-	int id;
+	long ultimoRecebido;
+
+	int nextID;
+
+	Set<Integer> idsRecebidos;
 
 	Servidor(InetAddress ipAddress){
 		this.ipAddress = ipAddress;
+		idsRecebidos = new HashSet<>();
+	}
+
+	InetAddress getIpAddress(){
+		return ipAddress;
+	}
+
+	float getPrioridade(){
+		if ((new Date()).getTime() - this.ultimoRecebido > 15000){
+			return Float.MAX_VALUE;
+		}
+		return rtt * conexoes * ((float) totalEnviados - idsRecebidos.size())/totalEnviados;
 	}
 
 	String enviar(){
-		Date d = new Date();
-		this.ultimoEnviado = d.getTime();
+		long tempoEnvio = (new Date()).getTime();
 		this.totalEnviados++;
-
-		return Integer.toString(id++);
+		return Integer.toString(nextID++) + " " + Long.toString(tempoEnvio);
 	}
 
-	void recebido(String id){
-		if (Integer.toString(this.id-1).equals(id)){
-			Date d = new Date();
-			long tempo = d.getTime();
-			rtt += (tempo - this.ultimoEnviado);
-			rtt /= 2;
-			this.totalRecebidos++;
+	void recebido(String stringID, String nTCP, String tempoEnvioExecucao){
+		int id = Integer.parseInt(stringID);
+		if (idsRecebidos.contains(id)){
+			return;
 		}
-	}
-}
-
-class Gestor extends Thread {
-	Tabela informacoes;
-
-	Gestor(Tabela informacoes){ this.informacoes = informacoes; }
-
-	public void run() {
-		while(true) {
-			try {
-				for(InetAddress key : informacoes.servidores.keySet()) {
-					Date d = new Date();
-					long tempo = d.getTime();
-					long diff = tempo - informacoes.servidores.get(key).ultimoEnviado;
-
-					System.out.println(key + ": RTT -> " + informacoes.servidores.get(key).rtt + "ms   \t LastActive: " + diff + "ms");
-					if(diff > 15000) informacoes.remover(key);
-				}
-				System.out.println("Número de Monitores Conectados: " + informacoes.servidores.size());
-				Thread.sleep(5000);
-			}
-			catch (InterruptedException e) {}
+		else{
+			idsRecebidos.add(id);
 		}
+		long tempoRececao = (new Date()).getTime();
+		this.ultimoRecebido = tempoRececao;
+			
+		long tempo = tempoRececao - Long.parseLong(tempoEnvioExecucao);
+			
+		if (rtt != 0.0f){
+			rtt = (rtt + tempo)/2;
+		}
+		else {
+			rtt = tempo;
+		}
+		this.conexoes = Integer.parseInt(nTCP);
 	}
+
+	void imprimirServidor(){
+		System.out.println(ipAddress + " - " + rtt + " - " + ((float) this.totalEnviados - this.idsRecebidos.size())/totalEnviados + " - " + conexoes);
+	}
+
 }
